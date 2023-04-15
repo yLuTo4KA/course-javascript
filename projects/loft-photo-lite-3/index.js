1e3e571cf02530b313be72c972eae119b5ccbaf7
Sergey Melyukov <smelukov@yandex-team.ru>
2023-04-15 16:57:06 +0300

    update

--- projects/loft-photo-lite-3/index.js
+++
@@ -1,8 +0,0 @@
-import pages from './pages';
-import mainPage from './mainPage';
-import loginPage from './loginPage';
-import('./styles.css');
-
-pages.openPage('login');
-loginPage.handleEvents();
-mainPage.handleEvents();
