11413eb7d6ee813b341a4e0471a260ee2a2d3915
Danil Hatin <kipsa25@mail.ru>
2023-09-25 13:01:56 +0600

    fix

--- projects/loft-photo/model.js
+++ projects/loft-photo/model.js
@@ -4,6 +4,20 @@ import photosDB from './photos.json';
 import friendsDB from './friends.json';
 
 export default {
-  getRandomElement(array) {},
-  getNextPhoto() {},
+  getRandomElement(array) {
+    if (!Array.isArray(array) || array.length == 0) {
+      throw new Error('empty array')
+  }
+  const rand = parseInt(Math.random() * array.length);
+  return array[rand]
+  },
+  getNextPhoto() {
+    const friend = this.getRandomElement(friendsDB);
+    const photosList = photosDB[friend.id];
+    const photo = this.getRandomElement(photosList);
+    return {
+        friend: friend.firstName,
+        url:  (photosList.length !== 0) ? photo.url : 'user photo not found'
+    }
+  },
 };
