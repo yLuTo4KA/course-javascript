c1737cc08fe48e009870b53a809948e88614b6ec
Danil Hatin <kipsa25@mail.ru>
2023-11-02 11:33:50 +0600

    add search friend

--- projects/loft-photo/profilePage.js
+++ projects/loft-photo/profilePage.js
@@ -18,7 +18,8 @@ export default {
 
         const userPhotos = await model.getPhotos(user['id']);
         const userFriends = await model.getFriends(user['id']);
-        
+        this.userFriendsList = userFriends.items;
+
         for (let i = 0; i < userPhotos.items.length; i++) {
             const currentPhoto = userPhotos.items[i];
             const div = document.createElement('div');
@@ -32,9 +33,16 @@ export default {
             })
             compUserPhotosContainer.appendChild(div);
         };
-        for(let i = 0; i < userFriends.count; i++){
+        this.setFriendsList(this.userFriendsList);
+
+
+    },
+    setFriendsList(friendsList) {
+        const compUserFriendsContainer = document.querySelector('.component-user-friends');
+        compUserFriendsContainer.innerHTML = '';
+        for (let friend of friendsList) {
             const friendsList = document.querySelector('.component-user-friends__list');
-            const currentFriend = userFriends.items[i];
+            const currentFriend = friend;
             const li = document.createElement('li');
             const div = document.createElement('div');
             div.classList.add('friend__photo');
@@ -43,7 +51,7 @@ export default {
             li.classList.add('friend', 'friend__item');
             li.innerHTML = friendTemplate;
             li.prepend(div);
-            li.addEventListener('click', async(e)=>{
+            li.addEventListener('click', async (e) => {
                 e.preventDefault();
                 const currentFriendPhotos = await model.getFriendPhotos(currentFriend.id);
                 const randomPhotos = model.getRandomElement(currentFriendPhotos.items);
@@ -53,11 +61,7 @@ export default {
                 location.hash = '#main';
             })
             friendsList.appendChild(li);
-            
-
         }
-
-
     },
 
     handleEvents() {
@@ -71,22 +75,39 @@ export default {
             await model.logout();
             location.hash = '#login';
         });
-        document.querySelector('.page-profile-photos').addEventListener('click', async (e)=>{
+        document.querySelector('.page-profile-photos').addEventListener('click', async (e) => {
             e.preventDefault();
             const currentEl = e.target;
+            document.querySelector('.component-user-info-search').classList.add('hidden');
             document.querySelector('.page-profile-friends').classList.remove('link--active');
             currentEl.classList.add('link--active')
             document.querySelector('.component-user-photos').classList.remove('hidden');
             document.querySelector('.component-user-friends').classList.add('hidden');
         });
-        document.querySelector('.page-profile-friends').addEventListener('click', async (e)=>{
+        document.querySelector('.page-profile-friends').addEventListener('click', async (e) => {
             e.preventDefault();
             const currentEl = e.target;
+            document.querySelector('.component-user-info-search').classList.remove('hidden');
             document.querySelector('.page-profile-photos').classList.remove('link--active');
             currentEl.classList.add('link--active')
             document.querySelector('.component-user-photos').classList.add('hidden');
             document.querySelector('.component-user-friends').classList.remove('hidden');
         });
-        
+        document.querySelector('#search-friend').addEventListener('input', async (e) => {
+            const valueInput = ((document.querySelector('#search-friend').value).toLowerCase()).trim();
+            if (valueInput !== '') {
+                let friendsSort = [];
+                for (let friend of this.userFriendsList) {
+                    if (friend.last_name.toLowerCase().includes(valueInput) || friend.first_name.toLowerCase().includes(valueInput)) {
+                        friendsSort.push(friend);
+                    }
+                }
+                this.setFriendsList(friendsSort);
+            } else {
+                this.setFriendsList(this.userFriendsList)
+            }
+
+        });
+
     }
 }
